93e4819f2b95c74790b198514cd96301f785df24
rvlouie <rvlouie@gmail.com>
2020-03-20 13:53:15 -0400

    Iterative updates from feedback

--- script.js
+++ script.js
@@ -8,24 +8,38 @@ $(document).ready(function() {
     $(".content__timeline__nav-item--past").click(function() {
         $(".content__timeline__wrapper--past").toggleClass("content__timeline__wrapper--open")
     });
+
     $(".icon-button--upload").click(function() {
         $(".content__add__upload-ui").addClass("content__add__upload-ui--open")
     });
     $(".content__add__close__upload-ui").click(function() {
         $(".content__add__upload-ui").removeClass("content__add__upload-ui--open")
     });
+
     $(".icon-button--write").click(function() {
         $(".content__add__writing").addClass("content__add__writing--open")
     });
     $(".content__add__close__writing").click(function() {
         $(".content__add__writing").removeClass("content__add__writing--open")
     });
+
+    $(".icon-button--schedule").click(function() {
+        $(".content__add__schedule-ui").addClass("content__add__schedule-ui--open")
+    });
+    $(".content__add__close__schedule-ui").click(function() {
+        $(".content__add__schedule-ui").removeClass("content__add__schedule-ui--open")
+    });
+
     $(".header__calendar-button").click(function() {
         $(".header__calendar-ui").toggleClass("header__calendar-ui--open")
     })
     $(".header__calendar-ui__close").click(function() {
         $(".header__calendar-ui").removeClass("header__calendar-ui--open")
     })
+
+    $(".content__upcoming__close").click(function() {
+        $(".content__upcoming").addClass("hide")
+    });
 
     // Panel
 
